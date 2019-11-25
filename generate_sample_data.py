6bd7afa0c50613370f982e5335e653d3e84454a4
Jonas Lindström <52708794+jdl00@users.noreply.github.com>
2019-11-25 14:24:04 +0000

    began working on generating sample data

--- generate_sample_data.py
+++ generate_sample_data.py
@@ -1,14 +1,31 @@
-import random
+import json as js
+import random as rand
+'''
+we create a class which deals with the write functions 
+and the generate functions along with allignement and 
+JSON parsing
+'''
+class data_generator:
+	def __init__(self):
+		self.__num_data_entries__ = int(0)
+		self.__sample_names__ = []
+
+	def write_line(self,string_to_write):
+
+	def generated_sample_data(self):
+
+	def parse_JSON(self):
+
+	def write_sample_data(self):
+		self.__num_data_entries__ = int(input("enter number of entries to enter"))
+		for i in range(self.__num_data_entries__):
+			write_line(self,generated_sample_data(self))
 
 
-def generate_record_data():
-    randint()
-
-def generate_txt_file():
-	file = open("")
 
 def main():
-   
+   sample_data_generator = data_generator()
+   sample_data_generator.write_sample_data()
 
 if __name__ == "__main__":
     main()
